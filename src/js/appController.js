75cc594d92fac1d57df924a25741943992ffd330
lprogg <lprogg>
2021-11-08 21:34:28 +0200

    Initial Setup

---
+++ src/js/appController.js
@@ -0,0 +1,75 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2021, Oracle and/or its affiliates.
+ * Licensed under The Universal Permissive License (UPL), Version 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/
+ * @ignore
+ */
+/*
+ * Your application specific code will go here
+ */
+define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils', 'ojs/ojcorerouter', 'ojs/ojmodulerouter-adapter', 'ojs/ojknockoutrouteradapter', 'ojs/ojurlparamadapter', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojarraydataprovider',
+        'ojs/ojoffcanvas', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+  function(ko, Context, moduleUtils, KnockoutTemplateUtils, CoreRouter, ModuleRouterAdapter, KnockoutRouterAdapter, UrlParamAdapter, ResponsiveUtils, ResponsiveKnockoutUtils, ArrayDataProvider, OffcanvasUtils) {
+
+     function ControllerViewModel() {
+
+      this.KnockoutTemplateUtils = KnockoutTemplateUtils;
+
+      // Handle announcements sent when pages change, for Accessibility.
+      this.manner = ko.observable('polite');
+      this.message = ko.observable();
+      announcementHandler = (event) => {
+          this.message(event.detail.message);
+          this.manner(event.detail.manner);
+      };
+
+      document.getElementById('globalBody').addEventListener('announce', announcementHandler, false);
+
+
+      // Media queries for repsonsive layouts
+      const mdQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+      this.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+
+      let navData = [
+        { path: '', redirect: 'home' },
+        { path: 'home', detail: { label: 'Home', iconClass: 'oj-ux-ico-home' } },
+        { path: 'about', detail: { label: 'About Us', iconClass: 'oj-ux-ico-information-s' } },
+        { path: 'menu', detail: { label: 'Menu', iconClass: 'oj-ux-ico-book' } },
+        { path: 'reservation', detail: { label: 'Reservation', iconClass: 'oj-ux-ico-library' } }
+      ];
+
+      // Router setup
+      let router = new CoreRouter(navData, {
+        urlAdapter: new UrlParamAdapter()
+      });
+      router.sync();
+
+      this.moduleAdapter = new ModuleRouterAdapter(router);
+
+      this.selection = new KnockoutRouterAdapter(router);
+
+      // Setup the navDataProvider with the routes, excluding the first redirected
+      // route.
+      this.navDataProvider = new ArrayDataProvider(navData.slice(1), {keyAttributes: "path"});
+
+      // Drawer
+      // Close offcanvas on medium and larger screens
+      this.mdScreen.subscribe(() => {OffcanvasUtils.close(this.drawerParams);});
+      this.drawerParams = {
+        displayMode: 'push',
+        selector: '#navDrawer',
+        content: '#pageContent'
+      };
+      // Called by navigation drawer toggle button and after selection of nav drawer item
+      this.toggleDrawer = () => {
+        this.navDrawerOn = true;
+        return OffcanvasUtils.toggle(this.drawerParams);
+      }
+     }
+     // release the application bootstrap busy state
+     Context.getPageContext().getBusyContext().applicationBootstrapComplete();
+
+     return new ControllerViewModel();
+  }
+);
